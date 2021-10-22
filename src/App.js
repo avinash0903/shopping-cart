ebc65245fc61ab06ae56a64ffe46cbc36a28436a
Avinash Eluka <avinash.skzr@gmail.com>
2021-10-22 17:24:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>All Rights Reserved..</footer>
+    </div>
+  );
+}
+
+export default App;
